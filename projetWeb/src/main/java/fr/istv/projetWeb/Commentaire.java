40491d9bdd44c3f85924d48c92aa352f34dd35cb
Angele-Guery <Angele-Guery@users.noreply.github.com>
2020-11-22 19:05:27 +0100

    tables
    tables créées

--- projetWeb/src/main/java/fr/istv/projetWeb/Commentaire.java
+++ projetWeb/src/main/java/fr/istv/projetWeb/Commentaire.java
@@ -1,26 +1,22 @@
 package fr.istv.projetWeb;
 
 import java.sql.Date;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
+	
 @Entity
-public class Bug {
+public class Commentaire {
 	@Id @GeneratedValue(strategy=GenerationType.AUTO) //Automatique
-	private int idBug;
-	private String titre;
-	private String description;
-	private String priorite;
-	private String avancement;
-	private Date dateCreation;
+	private int idCom;
+	private String message;
 	@ManyToOne
-	private Developpeur developpeur;
-	@OneToMany
-	private List<Commentaire> commentaire;
+	private Developpeur auteur;
+	private Date date;
+	@ManyToOne
+	private Bug bug;
 }
--- projetWeb/src/main/java/fr/istv/projetWeb/Developpeur.java
+++ projetWeb/src/main/java/fr/istv/projetWeb/Developpeur.java
@@ -1,26 +1,23 @@
 package fr.istv.projetWeb;
 
-import java.sql.Date;
+import java.awt.Image;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
-public class Bug {
+public class Developpeur {
 	@Id @GeneratedValue(strategy=GenerationType.AUTO) //Automatique
-	private int idBug;
-	private String titre;
-	private String description;
-	private String priorite;
-	private String avancement;
-	private Date dateCreation;
-	@ManyToOne
-	private Developpeur developpeur;
+	private int idDev;
+	private String nom;
+	private Image avatar;
 	@OneToMany
 	private List<Commentaire> commentaire;
+	@OneToMany
+	private List<Bug> bug;
+	
 }
