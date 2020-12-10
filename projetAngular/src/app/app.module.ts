2f98dfb3ec9b0f194d422b9845d667f8ae4ea265
Dylan-Helin <Dylan-Helin@users.noreply.github.com>
2020-12-10 11:13:17 +0100

    test componant angular

--- projetAngular/src/app/app.module.ts
+++ projetAngular/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListeTodoComponent } from './liste-todo/liste-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeTodoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,ListeTodoComponent]
 })
 export class AppModule { }
---
+++ projetAngular/src/app/liste-todo/liste-todo.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListeTodoComponent } from './liste-todo.component';
+
+describe('ListeTodoComponent', () => {
+  let component: ListeTodoComponent;
+  let fixture: ComponentFixture<ListeTodoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListeTodoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListeTodoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
