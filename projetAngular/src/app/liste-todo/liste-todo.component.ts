104d593eb804d627ffffac6bdb2da9cdaeaed38d
Angele-Guery <55349187+Angele-Guery@users.noreply.github.com>
2020-12-10 17:24:40 +0100

    test
    test  pour html et affichage des bugs

--- projetAngular/src/app/liste-todo/liste-todo.component.ts
+++ projetAngular/src/app/liste-todo/liste-todo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BugServiceService } from '../bug-service.service';
+import { Bug } from '../models/bug';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-liste-todo',
@@ -8,10 +11,67 @@ import { Component, OnInit } from '@angular/core';
 export class ListeTodoComponent implements OnInit {
 
 
-  constructor() { }
+  public bugs: Bug[];
+
+  constructor(private BugService: BugServiceService) { }
 
   ngOnInit(): void {
-    
+    this.BugService.getBugsList()
+    .pipe(delay(2000))
+    .subscribe((bugsResponse => {
+      this.bugs = bugsResponse;
+    }));
+  }
+
+
+
+}
+
+
+/******* getBugsList(): Observable<Bug[]>*/
+
+
+/*import { StudentsService } from './../services/students.service';
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../models/student';
+import { delay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-student-list',
+  templateUrl: './student-list.component.html',
+  styleUrls: ['./student-list.component.css']
+})
+export class StudentListComponent implements OnInit {
+
+
+  public students: Student[];
+
+  public showSpinner: boolean;
+
+  constructor(private studentsService: StudentsService) { }
+
+  ngOnInit(): void {
+    this.showSpinner = true;
+    this.studentsService.getStudentsList()
+    .pipe(delay(2000))
+    .subscribe((studentsResponse => {
+      this.students = studentsResponse;
+      this.showSpinner = false;
+    }));
+  }
+
+  deleteStudent(id: number): void {
+    this.studentsService.deleteStudent(id).subscribe((deleteResponse) => {
+      this.students = this.students.filter(s => s.id !== id);
+    }, (error) => {
+      // TODO
+    });
+  }
+  /* Callback of child component */
+  /*
+  addStudent(newStudent: Student): void {
+    this.students.push(newStudent);
   }
 
 }
+*/
