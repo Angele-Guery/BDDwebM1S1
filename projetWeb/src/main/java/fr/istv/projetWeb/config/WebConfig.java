512ce0d424823ecb5fad294951e6f4cbabe4f5d3
MrADOY <pietrzak.aurelien@gmail.com>
2020-12-10 19:01:25 +0100

    fix cors

--- projetWeb/src/main/java/fr/istv/projetWeb/config/WebConfig.java
+++ projetWeb/src/main/java/fr/istv/projetWeb/config/WebConfig.java
@@ -1,4 +1,4 @@
-package config;
+package fr.istv.projetWeb.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
